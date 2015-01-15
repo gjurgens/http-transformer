ffc3b8600dec7757d8ca25974c9512dd03a3c308
gjurgens <gjurgens@despegar.com>
2015-01-15 17:47:38 -0300

    Adding content transformers

--- testServer.js
+++ testServer.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('request successfully proxied: ' + req.headers['host'] + "|" + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body>\n<h1 class="foo">Test Response</h1>\n<pre>request successfully proxied: ' + req.headers['host'] + "|" + req.url + '\n' + JSON.stringify(req.headers, true, 2) + "</pre>\n</pre></body></body></html>");
     res.end();
 }).listen(9000);
