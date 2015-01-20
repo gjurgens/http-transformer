445ac6401ba39d0021bdfb159a651167cc756f7e
gjurgens <gjurgens@despegar.com>
2015-01-20 18:00:20 -0300

    Preparing for command line options

--- app.js
+++ app.js
@@ -1,49 +1,57 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+var _ = require("underscore");
 
-var controlPanelRoute = require('./routes/controlPanel');
+module.exports = function(_options){
+    var express = require('express');
+    var path = require('path');
+    var logger = require('morgan');
 
-var app = express();
-var proxy = require("./lib/proxy");
+    var controlPanelRoute = require('./routes/controlPanel');
 
-var requestId = 1;
+    var app = express();
+    var proxy = require("./lib/proxy");
 
-app.set("x-powered-by",false);
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    var options = {
+        "port":app.get("port")||3000,
+        "ssl_port":app.get("ssl_port")||3001,
+        "rules":"rules"
+    };
+    _.extend(options,_options);
 
-app.use(logger('dev'));
+    var requestId = 1;
 
-app.use(function(req, res, next){
-    req.id = requestId++;
-    next();
-});
+    app.set("x-powered-by",false);
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
 
-app.use(proxy({
-    "port":app.get("port")||3000,
-    "ssl_port":app.get("ssl_port")||3001
-}));
+    app.use(logger('dev'));
 
-app.use(express.static(path.join(__dirname, 'static')));
-
-app.use('/', controlPanelRoute);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// handlers
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
+    app.use(function(req, res, next){
+        req.id = requestId++;
+        next();
     });
-});
 
-module.exports = app;
+    app.use(proxy(options));
+
+    app.use(express.static(path.join(__dirname, 'static')));
+
+    app.use('/', controlPanelRoute);
+
+    /// catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    /// handlers
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+
+    return app;
+};
